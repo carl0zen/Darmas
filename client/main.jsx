4e8a561801ef6e038f168052fa7381789171d707
Carlos Perez <carlosepp@gmail.com>
2015-03-21 03:34:44 -0600

    Implement Twitter Login React Component

---
+++ client/main.jsx
@@ -0,0 +1,131 @@
+/** @jsx React.DOM */
+ThirdEye = {};
+
+Comments = new Meteor.Collection('comments');
+
+Meteor.startup(function() {
+  var converter = new Showdown.converter();
+
+  var Comment = React.createClass({
+    render: function() {
+      var rawMarkup = converter.makeHtml(this.props.children.toString());
+      return (
+        <div className="comment">
+         <h2 className="commentAuthor">{this.props.author}</h2>
+         <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+        </div>
+      );
+    }
+  });
+
+  var CommentBox = React.createClass({
+    handleCommentSubmit: function(comment) {
+      var comments = this.state.data;
+      comments.push(comment);
+      this.setState({data: comments});
+      comment._id = Comments.insert(comment);
+    },
+    getInitialState: function() {
+      return {data: []};
+    },
+    render: function() {
+      return (
+        <div className="commentBox">
+          <h1>Comments</h1>
+          <CommentList data={this.state.data} />
+          <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+        </div>
+      );
+    }
+  });
+
+  var CommentList = React.createClass({
+    mixins: [MeteorMixin],
+    getMeteorState: function() {
+      return {
+        data: Comments.find({}).fetch()
+      };
+    },
+    render: function() {
+      var commentNodes = this.state.data.map(function (comment) {
+        return <Comment key={comment._id} author={comment.author}>{comment.text}</Comment>;
+      });
+      return <div className="commentList">{commentNodes}</div>;
+    }
+  });
+
+  var CommentForm = React.createClass({
+    handleSubmit: function() {
+      var author = this.refs.author.getDOMNode().value.trim();
+      var text = this.refs.text.getDOMNode().value.trim();
+      this.props.onCommentSubmit({author: author, text: text});
+      this.refs.author.getDOMNode().value = '';
+      this.refs.text.getDOMNode().value = '';
+      return false;
+    },
+    render: function() {
+      return (
+        <form className="commentForm" onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Your name" ref="author" />
+          <textarea type="text" placeholder="Say something..." ref="text"></textarea>
+          <input type="submit" value="Post" />
+        </form>
+      );
+    }
+  });
+
+
+// <CommentBox url="/comments.json" pollInterval={2000} />,
+  React.renderComponent(
+    <CommentBox />,
+    document.getElementById('container')
+  );
+
+  var LoginWithTwitter = React.createClass({
+    getInitialState: function() {
+      return {loggedIn: false};
+    },
+
+    login: function  (e) {
+      var that = this;
+      Meteor.loginWithTwitter(function() {
+        that.setState({ loggedIn: true });
+      });
+    },
+
+    logout: function  () {
+      var that = this;
+      Meteor.logout(function() {
+        that.setState({ loggedIn: false });
+      });
+    },
+
+    render: function() {
+      if (this.state.loggedIn) {
+        var twitterUser = Meteor.user().services.twitter;
+
+        return(
+          <div>
+            <a href={ "https://twitter.com/" + twitterUser.screenName } >
+              <img src={ twitterUser.profile_image_url } />{ twitterUser.screenName }
+            </a>
+            <a onClick={ this.logout }>Logout</a>
+          </div>
+        );
+      } else {
+        return(
+          <a onClick={ this.login }>
+            Login
+          </a>
+        );
+      }
+    }
+  });
+
+  React.renderComponent(
+    <LoginWithTwitter />,
+    document.getElementById('twitter')
+  );
+});
+
+
---
+++ server/main.js
@@ -0,0 +1,5 @@
+Comments = new Meteor.Collection('comments');
+Comments.remove({});
+
+var url = Npm.require("url");
+
